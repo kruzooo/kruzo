551dedf840c3986f773aec2957db0ee84d5a90d4
Pryvst Pedrera <pryvstpedrera@gmail.com>
2026-09-09 14:11:24 +0800

    Avoid malformed database URL parsing

--- vibe-app/config/database.php
+++ vibe-app/config/database.php
@@ -86,7 +86,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
+            'url' => null,
             'host' => env('DB_HOST', env('PGHOST', '127.0.0.1')),
             'port' => env('DB_PORT', env('PGPORT', '5432')),
             'database' => env('DB_DATABASE', env('PGDATABASE', 'laravel')),
